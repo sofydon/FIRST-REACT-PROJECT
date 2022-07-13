b01b4600a572acb428a76017ed3f6dc134d6a46f
sofydon <petinova@bk.ru>
2022-07-12 20:56:54 -0400

    added data

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -1,14 +1,20 @@
 import './employees-list-item.css';
 
-const EmployeesListItem = ({name, salary}) => {
+const EmployeesListItem = ({name, salary, increase}) => {
+
+    let classNames = "list-group-item d-flex justify-content-between";
+    if(increase) {
+        classNames += ' increase'
+
+    }
     return (
-        <li className="list-group-item d-flex justify-content-between">
+        <li className={classNames}>
             <span className="list-group-item-label">{name}</span>
             <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
             <div className='d-flex justify-content-center align-items-center'>
                 <button type="button"
                     className="btn-cookie btn-sm ">
-                    <i className="fas fa-cookie"></i>
+                    <i className="fas fa-cookie">{increase}</i>
                 </button>
 
                 <button type="button"
--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -2,12 +2,16 @@ import EmployeesListItem from "../employees-list-item/employees-list-item";
 
 import './employees-list.css';
 
-const EmployeesList = () => {
+const EmployeesList = ({data}) => {
+
+    const elements = data.map(item => {
+        return (
+            <EmployeesListItem name={item.name} salary={item.salary} increase={item.increase}/>
+        )
+    })
     return (
         <ul className="app-list list-group">
-            <EmployeesListItem name="John S" salary={800}/>
-            <EmployeesListItem name="Alex M" salary={1000}/>
-            <EmployeesListItem name="Carl W" salary={5000}/>
+            {elements}
         </ul>
     )
 }
