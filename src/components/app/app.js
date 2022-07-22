67255428cc125813986eb6db124b70a60fef7907
sofydon <petinova@bk.ru>
2022-07-22 19:38:09 -0400

    added func to appinfo

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -13,13 +13,12 @@ class App extends Component {
     super(props);
     this.state = {
       data: [
-        {name: 'John S.', salary: 800, id:1},
-        {name: 'Alex M.', salary: 1000, id:2},
-        {name: 'Carl W.', salary: 5000, id:3}
+        {name: 'John S.', salary: 800, increase:false, rise:true, id:1},
+        {name: 'Alex M.', salary: 1000, increase:true, rise:false, id:2},
+        {name: 'Carl W.', salary: 5000, increase:false, rise:false, id:3}
       ]
     }
     this.maxId = 4;
-
   }
 
   deleteItem = (id)=>{
@@ -31,11 +30,12 @@ class App extends Component {
     })
   }
 
-  addItem = (name,salary)=>{
+  addItem = (name,salary) => {
     const newItem = {
       name,
       salary,
       increase: false,
+      rise: false,
       id: this.maxId++
     }
     this.setState(({data}) => {
@@ -45,11 +45,47 @@ class App extends Component {
       }
     })
   }
+
+  onToggleIncrease = (id) => {
+    // this.setState(({data})=> {
+    //   const index = data.findIndex(elem => elem.id === id);
+    //   const old = data[index];
+    //   const newItem = {...old, increase: !old.increase};
+    //   const newArr = [...data.slice(0, index), newItem, ...data.slice(index + 1)];
+    //   return {
+    //     data: newArr
+    //   }
+    // })
+    this.setState(({data})=> ({
+      data: data.map(item =>{
+        if(item.id === id) {
+          return {...item, increase: !item.increase}
+        }
+        return item;
+      })
+    }))
+  }
+
+  onToggleRise = (id) => {
+    this.setState(({data})=> ({
+      data: data.map(item => {
+        if(item.id === id) {
+          return {...item, rise: !item.rise}
+        }
+        return item;
+      })
+    }))
+  }
   
   render() {
+    const employees = this.state.data.length;
+    const increased = this.state.data.filter(item => item.increase).length;
+    
     return (
       <div className="app">
-          <AppInfo />
+          <AppInfo 
+          employees={employees}
+          increased={increased}/>
   
           <div className="search-panel">
               <SearchPanel/>
@@ -58,7 +94,9 @@ class App extends Component {
           
           <EmployeesList 
           data={this.state.data}
-          onDelete={this.deleteItem}/>
+          onDelete={this.deleteItem}
+          onToggleIncrease={this.onToggleIncrease}
+          onToggleRise={this.onToggleRise}/>
           <EmployeesAddForm
           onAdd={this.addItem}/>
       </div>
