e7873ac5bc4809d063269ce58b9993272caeacf3
sofydon <petinova@bk.ru>
2022-07-22 21:14:23 -0400

    optimized onToggle func

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -46,7 +46,7 @@ class App extends Component {
     })
   }
 
-  onToggleIncrease = (id) => {
+  onToggleProp = (id, prop) => {
     // this.setState(({data})=> {
     //   const index = data.findIndex(elem => elem.id === id);
     //   const old = data[index];
@@ -59,23 +59,23 @@ class App extends Component {
     this.setState(({data})=> ({
       data: data.map(item =>{
         if(item.id === id) {
-          return {...item, increase: !item.increase}
+          return {...item, [prop]: !item[prop]}
         }
         return item;
       })
     }))
   }
 
-  onToggleRise = (id) => {
-    this.setState(({data})=> ({
-      data: data.map(item => {
-        if(item.id === id) {
-          return {...item, rise: !item.rise}
-        }
-        return item;
-      })
-    }))
-  }
+  // onToggleRise = (id) => {
+  //   this.setState(({data})=> ({
+  //     data: data.map(item => {
+  //       if(item.id === id) {
+  //         return {...item, rise: !item.rise}
+  //       }
+  //       return item;
+  //     })
+  //   }))
+  // }
   
   render() {
     const employees = this.state.data.length;
@@ -95,8 +95,7 @@ class App extends Component {
           <EmployeesList 
           data={this.state.data}
           onDelete={this.deleteItem}
-          onToggleIncrease={this.onToggleIncrease}
-          onToggleRise={this.onToggleRise}/>
+          onToggleProp={this.onToggleProp}/>
           <EmployeesAddForm
           onAdd={this.addItem}/>
       </div>
