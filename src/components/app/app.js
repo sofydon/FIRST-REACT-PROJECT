2e5eba6d11f6e89557e13b3fb96c130bef3bf5cd
sofydon <petinova@bk.ru>
2022-07-24 10:08:02 -0400

    filter func

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -17,7 +17,8 @@ class App extends Component {
         {name: 'Alex M.', salary: 1000, increase:true, rise:false, id:2},
         {name: 'Carl W.', salary: 5000, increase:false, rise:false, id:3}
       ],
-      term: ''
+      term: '',
+      filter: 'rise'
     }
     this.maxId = 4;
   }
@@ -89,12 +90,23 @@ class App extends Component {
   onUpdateSearch = (term) => {
     this.setState({term})
   }
-  
+
+  onFilterEmp = (items, filter) => {
+    switch(filter) {
+        case 'rise':
+          return items.filter(item => item.rise);
+        case 'more than 1000':
+          return items.filter(item => item.salary > 1000);
+        default: 
+          return items
+    }
+  }
+
   render() {
-    const {data, term} = this.state;
+    const {data, term, filter} = this.state;
     const employees = this.state.data.length;
     const increased = this.state.data.filter(item => item.increase).length;
-    const visibleData = this.searchEmp(data, term);
+    const visibleData = this.onFilterEmp(this.searchEmp(data, term), filter);
     
     return (
       <div className="app">
